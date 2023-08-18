fd9c2cc382edee11d7a632c6f742ecd38715e1be
paju3125 <prajvalgandhi483@gmail.com>
2023-08-18 14:54:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Hello World API
+
+Checkout my solution by clicking on this link
+https://prajval.pythonanywhere.com/
+
+
+
+---
+## Installation
+
+
+1. Create a virtual environment
+```bash
+  python -m venv venv
+```
+
+2. Activate environment
+```bash
+  .\venv\Scripts\activate
+```
+
+3. Install dependencies
+```bash
+  pip install -r requirements.txt
+```
+
+4. Run flask webapp
+```bash
+  python app.py
+```
+
+5. Interact with webpage
+```bash
+  Click on - http://localhost:5000
+```
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# User homepage for selecting language and making get request using without reloading page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# hello api
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    
+    # Accepting the language input from get request
+    language = request.args.get('language', None)
+    
+    # If language parameter is not passed as argument
+    if language is None:
+        error_response = {
+            "error_message": "Language parameter is required"
+        }
+        return jsonify(error_response), 400
+    
+    # Dictionary containing language name along with translation
+    greetings = {
+        'english': 'Hello world',
+        'french': 'Bonjour le monde',
+        'hindi': 'Namastey sansar'
+    }
+    
+    language = language.lower()
+    
+    # checks whether the language given is supported
+    # If yes, then end the JOSN containing ID and the translated message
+    if language in greetings:
+        response_data = {
+            "ID" : "A123456789",
+            "msgText": greetings[language]
+        }
+        return jsonify(response_data), 200
+    else:
+        # runs when the language is not supported by api
+        error_response = {
+            "error_message": "The requested language is not supported"
+        }
+        return jsonify(error_response), 400
+
+if __name__ == '__main__':
+    app.run(host="localhost", debug=True)
